a195eabfba3d053adb583c35e569a5076db84469
mayurg22 <78292498+mayurg22@users.noreply.github.com>
2024-03-24 13:02:28 +0530

    added dotEnv file logic

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const server = express();
 
-const port = 3000
+const NODE_ENV = process.env.NODE_ENV;
+
+// .env file handling
+const path = require("path")
+require('dotenv').config({ path: path.resolve(__dirname, `./config/.${NODE_ENV}`) })
+
+// console.log(process.env.port)
+const port = process.env.port || 3000
+
+// console.log("port", port)
 
 server.use("/healthCheck", function (req, res) {
     res.send(`Server is working !!`)
