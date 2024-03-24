2ab1cf43d8138dcc8cc045efdb4208d641fed34c
mayurg22 <78292498+mayurg22@users.noreply.github.com>
2024-03-24 14:17:58 +0530

    renaming: location to locations

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const router = Router();
 const location = require("./src/modules/location/locationRoutes")
 const weather = require("./src/modules/weather/weatherRoutes")
 
-router.use("/location", location)
+router.use("/locations", location)
 router.use("/weather", weather)
 
 module.exports = router;
