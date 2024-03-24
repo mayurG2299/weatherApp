28ff4b19534d2dd9df2eaa317e4beb3465841ccf
mayurg22 <78292498+mayurg22@users.noreply.github.com>
2024-03-24 20:41:19 +0530

    restructured location controller calls

--- src/modules/location/locationController.js
+++ src/modules/location/locationController.js
@@ -5,7 +5,7 @@ module.exports.addLocation = async (req, res) => {
     await addLocationSchema.validateAsync(req.body);
     const { name, latitude, longitude } = req.body;
     const location = await Location.create({ name, latitude, longitude });
-    console.log(location)
+    // console.log(location)
     return location;
 }
 
@@ -13,27 +13,26 @@ module.exports.getLocationByID = async (req, res, next) => {
     const { location_id } = req.params;
     const location = await Location.findByPk(location_id);
     console.log("location", location)
+    if (!location) { let err = new Error(`No record found with lcoation id ${location_id}`); err.statusCode = 404; throw err }
     return location;
 }
 
 module.exports.updateLocationByID = async (req, res, next) => {
     const { location_id } = req.params;
     const { name, latitude, longitude } = req.body
-    const location = await Location.update({ name, latitude, longitude }, {
-        where: {
-            id: location_id,
-        },
-    });
+    let location = await Location.findByPk(location_id);
+    console.log("location", location)
+    if (!location) { let err = new Error(`No record found with lcoation id ${location_id}`); err.statusCode = 404; throw err }
+    location = await location.update({ name, latitude, longitude });
     return location;
 }
 
 module.exports.deleteLocationBYID = async (req, res, next) => {
     const { location_id } = req.params;
-    const location = await Location.destroy({
-        where: {
-            id: location_id,
-        }
-    });
+    let location = await Location.findByPk(location_id);
+    console.log("location", location)
+    if (!location) { let err = new Error(`No record found with lcoation id ${location_id}`); err.statusCode = 404; throw err }
+    location = await location.destroy();
     return location;
 }
 
